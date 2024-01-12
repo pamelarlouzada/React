001f380bf475f4d92d4f097a3598c0292a559d88
pamela.louzada <pamela.rlouzada@gmail.com>
2024-01-12 16:23:58 -0300

    Lifecycle methods (métodos de ciclo de vida) - Parte 1

--- src/App.js
+++ src/App.js
@@ -2,39 +2,65 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-    state = {
-      posts: [
-        {
-          id: 1,
-          title: 'O título 1',
-          body: 'O corpo 1'
-        },
-        {
-          id: 2,
-          title: 'O título 2',
-          body: 'O corpo 2'
-        },
-        {
-          id: 3,
-          title: 'O título 3',
-          body: 'O corpo 3'
-        },
-      ]
-    };
+  state = {
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: 'O título 1',
+        body: 'O corpo 1'
+      },
+      {
+        id: 2,
+        title: 'O título 2',
+        body: 'O corpo 2'
+      },
+      {
+        id: 3,
+        title: 'O título 3',
+        body: 'O corpo 3'
+      },
+    ]
+  };
 
-  render () {
-    const { posts } = this.state; // same thing that: `const name = this.state.name;` study=(destructuring assignment by javascript) study=(arrow function by javascript)
+  timeoutUpdate = null;
+
+  componentDidMount() {
+    this.handleTimeout();
+  }
+
+  componentDidUpdate(){
+    this.handleTimeout();
+  }
+
+  componentWillUnmount(){
+    clearTimeout(this.timeoutUpdate);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = 'O título mudou';
+
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 })
+    }, 1000);
+  }
+
+  render() {
+    const { posts, counter } = this.state; 
     return (
-          // study=(Array.prototype.map() by javascript)
-          <div className="App">
-            {posts.map(post => (
-              <div key={post.id}>
-                <h1>{post.title}</h1>
-                <p>{post.body}</p>
-              </div>
-            ))} 
+      
+      <div className="App">
+        <h1>{counter}</h1>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
           </div>
-        );
+        ))}
+      </div>
+      
+    );
   }
 }
 
